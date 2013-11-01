c8aa0a6fa5bb00c12885374a84b7bbb350e53806
Timo Sulg <timgluz@gmail.com>
2013-11-01 15:45:00 +0100

    Added clojure branch;

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clj-http "0.1.1"
+  :description
+    "A Clojure HTTP library wrapping the Apache HttpComponents client."
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]
+     [org.apache.httpcomponents/httpclient "4.0.1"]
+     [commons-codec "1.4"]
+     [commons-io "1.4"]]
+  :dev-dependencies
+    [[swank-clojure "1.2.0"]
+     [ring/ring-jetty-adapter "0.2.5"]
+     [ring/ring-devel "0.2.5"]])
